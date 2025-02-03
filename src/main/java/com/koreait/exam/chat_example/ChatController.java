1abecb1442549a5f61e0ea1b803b33c427712357
biso15 <biso150@gmail.com>
2025-02-03 16:08:21 +0900

    메세지 저장

--- src/main/java/com/koreait/exam/chat_example/ChatController.java
+++ src/main/java/com/koreait/exam/chat_example/ChatController.java
@@ -3,16 +3,30 @@ package com.koreait.exam.chat_example;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/chat") // class에 RequestMapping annotation 추가시 class의 모든 메서드에 적용
 public class ChatController {
 
-    ChatMessage message = new ChatMessage("김철수", "안녕");
+    private List<ChatMessage> chatMessages = new ArrayList<ChatMessage>();
+
+    public record writeChatMessageResponse(long id) {  // class 안에 class 가능
+
+    }
 
     @PostMapping("/writeMessage")
     @ResponseBody  // 화면에서 보려면 사용해야 하는 annotation
-    public RsData<ChatMessage> writeMessage() {
-        return new RsData<>("S-1", "메세지가 작성됨", message);
+    public RsData<writeChatMessageResponse> writeMessage() {
+
+        ChatMessage message = new ChatMessage("김철수", "안녕");
+        chatMessages.add(message);
+        return new RsData<>(
+            "S-1",
+            "메세지가 작성됨",
+            new writeChatMessageResponse(message.getId())
+        );
     }
 
 }
--- src/main/java/com/koreait/exam/chat_example/ChatMessage.java
+++ src/main/java/com/koreait/exam/chat_example/ChatMessage.java
@@ -14,6 +14,15 @@ public class ChatMessage {
     private String content;
 
     public ChatMessage(String authorName, String content) {
-        this(1, LocalDateTime.now(), authorName, content);  // 다른 생성자를 호출한다. -> AllArgsConstructor
+        this(ChatMessageIdGenerator.getNextId(), LocalDateTime.now(), authorName, content);  // 다른 생성자를 호출한다. -> AllArgsConstructor
+    }
+}
+
+class ChatMessageIdGenerator {
+
+    private static long id = 0;
+
+    public static long getNextId() {
+        return ++id;
     }
 }
