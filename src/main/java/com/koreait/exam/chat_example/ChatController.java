c2e3e2f0f936547a7bb852d41dec3b7045665def
biso15 <biso150@gmail.com>
2025-02-03 17:58:43 +0900

    json 형태로 데이터 전달, record

--- src/main/java/com/koreait/exam/chat_example/ChatController.java
+++ src/main/java/com/koreait/exam/chat_example/ChatController.java
@@ -1,5 +1,7 @@
 package com.koreait.exam.chat_example;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,15 +14,29 @@ public class ChatController {
 
     private List<ChatMessage> chatMessages = new ArrayList<ChatMessage>();
 
-    public record writeChatMessageResponse(long id) {  // class 안에 class 가능
+    // record가 아닐 때
+    @AllArgsConstructor
+    @Getter
+    public static class writeChatMessageRequest {  // class 안에 class 가능
+        private final String authorName;
+        private final String content;
+    }
+
+//    public record writeChatMessageRequest(String authorName, String content) {
+//
+//    }
+
+    public record writeChatMessageResponse(long id) {
 
     }
 
     @PostMapping("/writeMessage")
     @ResponseBody  // 화면에서 보려면 사용해야 하는 annotation
-    public RsData<writeChatMessageResponse> writeMessage() {
+    public RsData<writeChatMessageResponse> writeMessage(@RequestBody writeChatMessageRequest req) {  // record가 아닐 때
+//    public RsData<writeChatMessageResponse> writeMessage(@RequestBody writeChatMessageRequest req) {
 
-        ChatMessage message = new ChatMessage("김철수", "안녕");
+         ChatMessage message = new ChatMessage(req.authorName, req.content);  // record가 아닐 때
+//        ChatMessage message = new ChatMessage(req.authorName(), req.content());
         chatMessages.add(message);
         return new RsData<>(
             "S-1",
@@ -29,4 +45,31 @@ public class ChatController {
         );
     }
 
+    public record messagesResponse(List<ChatMessage> messages, long count) {
+
+    }
+
+    @GetMapping("/messages")
+    @ResponseBody  // 화면에서 보려면 사용해야 하는 annotation
+    public RsData<messagesResponse> messages() {
+        return new RsData<>(
+                "S-1",
+                "메세지 리스트",
+                new messagesResponse(chatMessages, chatMessages.size())
+        );
+    }
 }
+
+
+
+//    @GetMapping("/message")
+//    @ResponseBody  // 화면에서 보려면 사용해야 하는 annotation
+//    public RsData<List<ChatMessage>> messages() {
+////        ChatMessage message = new ChatMessage("김철수", "안녕");
+////        chatMessages.add(message);
+//        return new RsData<>(
+//                "S-1",
+//                "메세지가 작성됨",
+//                new writeChatMessageResponse(new ChatMessage)
+//        );
+//    }
